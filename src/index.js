66d1eb39e1975d7c53c5ce30efdca6fc1ce68bcb
Carlos <dschworld@gmail.com>
2025-02-11 00:08:38 -0400

    last edit 11/2/2025

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send('Bienvenido al sistema escolar!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port: http://localhost:${PORT}`);
 });
